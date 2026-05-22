942b3b57805737c2f0ca9590e53997b41b37f5a6
harutono-113 <harutono13@outlook.jp>
2026-05-22 10:18:21 +0900

    Add : ログイン画面のレイアウト修正とログイン処理を実装
    現状はログイン後の画面の実装はできていない。

--- src/repositories/auth.js
+++ src/repositories/auth.js
@@ -13,4 +13,15 @@ export const authRepository = {
       userName: data.user.user_metadata.name,
     };
   },
+  async signin(email, password) {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+    if (error) throw new Error(error.message);
+    return {
+      ...data.user,
+      userName: data.user.user_metadata,
+    };
+  },
 };
