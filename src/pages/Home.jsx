b3d92c3945baa10e41506f81adf54bd2525375be
harutono-113 <harutono13@outlook.jp>
2026-05-22 23:18:06 +0900

    Add : 投稿した内容をPost押下時にすぐに投稿欄の一番上に反映される処理を実装

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,31 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { SessionContext } from "../SessionProvider";
 import { Navigate } from "react-router-dom";
 import { SideMenu } from "../components/sideMenu";
+import { Post } from "../components/Post";
 import { postRepository } from "../repositories/post";
 
 function Home() {
   const [content, setContent] = useState("");
+  const [posts, setPosts] = useState([]);
   const { currentUser } = useContext(SessionContext);
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
   const createPost = async () => {
     const post = await postRepository.create(content, currentUser.id);
+    setPosts([
+      { ...post, userId: currentUser.id, userName: currentUser.userName },
+      ...posts,
+    ]);
     setContent("");
+  };
+
+  const fetchPosts = async () => {
+    const posts = await postRepository.find();
+    setPosts(posts);
   };
 
   if (currentUser == null) return <Navigate replace to="/signin" />;
@@ -40,7 +55,11 @@ function Home() {
                 Post
               </button>
             </div>
-            <div className="mt-4"></div>
+            <div className="mt-4">
+              {posts.map((post) => (
+                <Post key={post.id} post={post} />
+              ))}
+            </div>
           </div>
           <SideMenu />
         </div>
